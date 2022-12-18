eaf2cd11d66859643d1226ffb36aac13f08e6af6
zaneerina <zane.erina@gmail.com>
2022-12-18 20:13:27 +0200

    added another method for NumberHelper class without a unit test to check the coverage stats

--- Unittest/src/main/java/NumberHelper.java
+++ Unittest/src/main/java/NumberHelper.java
@@ -2,4 +2,7 @@ public class NumberHelper {
     public int addNumbers (int num1, int num2){
         return num1 + num2;
     }
+    public int subtractNumbers (int num1, int num2){
+        return num1 - num2;
+    }
 }
